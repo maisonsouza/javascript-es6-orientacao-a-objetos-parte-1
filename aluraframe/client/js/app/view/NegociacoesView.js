9eadf19f7327dc74adbebcc41c96840aa57dedc7
Maison Galvão <maisongalvao144@gmail.com>
2019-03-17 17:21:22 -0400

    Aula05 - Temos o modelo mas e a view

--- aluraframe/client/js/app/view/NegociacoesView.js
+++ aluraframe/client/js/app/view/NegociacoesView.js
@@ -28,13 +28,7 @@ class NegociacoesView {
                 </tbody>
                 <tfoot>
                     <td colspan="3"></td>
-                    <td>${
-                        (function () {
-                            let total = 0;
-                            model.negociacoes.forEach(negociacao => total += negociacao.volume);
-                            return total;
-                        })()}
-            
+                    <td>${model.negociacoes.reduce((total,n) =>  total + n.volume,0.0)}
                     </td>
                 </tfoot>
             </table>
